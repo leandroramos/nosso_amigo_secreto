85998035f565a3dd24c7a278a83265101469c5da
Leandro Ramos <leandroembu@gmail.com>
2018-02-03 15:00:20 -0200

    Creating Raffle Service and preparing the tests with Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,4 +55,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
